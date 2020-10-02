bd26f4dcb043160f28bb5ccba9e791a0cab91467
VKovrigina <v.kovrigina.2001@yandex.ru>
2020-10-02 09:55:15 +0300

    feat: add validation for Login & Register

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,38 @@
 import React from 'react';
+import { useFormWithValidation } from '../hooks/useFormWithValidation';
 
 function Login() {
+
+  const {values, handleChange, errors, isValid, resetForm} = useFormWithValidation();
+  React.useEffect(() => {
+    resetForm();
+  }, [resetForm]);
+
   return (
     <section className="login-page">
     <form className="form form_type_dark form_type_full-page">
       <h1 className="form__title form__title_type_light">Вход</h1>
       <input
+        value={values.email || ''}
+        onChange={handleChange}
         type="email"
         name="email"
         className="form__input form__input_type_light"
-        id="name-input"
         placeholder="Email"
         required />
-      <span className="form__span-error" id="name-input-error"></span>
+      <span className="form__span-error">{errors.email || ''}</span>
       <input
+        value={values.password || ''}
+        onChange={handleChange}
+        minLength="5"
+        maxLength="12"
         type="password"
         name="password"
         className="form__input form__input_type_light"
-        id="job-input"
         placeholder="Пароль"
         required />
-      <span className="form__span-error" id="job-input-error"></span>
-      <button className="form__button form__button_type_light form__button_type_login" type="submit" aria-label="Войти">Войти</button>
+      <span className="form__span-error">{errors.password || ''}</span>
+      <button className={`form__button form__button_type_light form__button_type_login ${!isValid && 'form__button_type_light_inactive'}`} type="submit" aria-label="Войти" disabled={!isValid}>Войти</button>
       <p className="form__question">Еще не зарегистрированы?
         <a className="form__link"> Зарегистрироваться</a>
       </p>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,27 +1,38 @@
 import React from 'react';
+import { useFormWithValidation } from '../hooks/useFormWithValidation';
 
 function Register() {
+
+  const {values, handleChange, errors, isValid, resetForm} = useFormWithValidation();
+  React.useEffect(() => {
+    resetForm();
+  }, [resetForm]);
+
   return (
     <section className="login-page">
     <form className="form form_type_dark form_type_full-page">
       <h1 className="form__title form__title_type_light">Регистрация</h1>
       <input
+        value={values.email || ''}
+        onChange={handleChange}
         type="email"
         name="email"
         className="form__input form__input_type_light"
-        id="name-input"
         placeholder="Email"
         required />
-      <span className="form__span-error" id="name-input-error"></span>
+      <span className="form__span-error">{errors.email || ''}</span>
       <input
+        value={values.password || ''}
+        onChange={handleChange}
         type="password"
         name="password"
+        minLength="5"
+        maxLength="12"
         className="form__input form__input_type_light"
-        id="job-input"
         placeholder="Пароль"
         required />
-      <span className="form__span-error" id="job-input-error"></span>
-      <button className="form__button form__button_type_light form__button_type_login" type="submit" aria-label="Зарегистрироваться">Зарегистрироваться</button>
+      <span className="form__span-error">{errors.password || ''}</span>
+      <button className={`form__button form__button_type_light form__button_type_login ${!isValid && 'form__button_type_light_inactive'}`} type="submit" aria-label="Зарегистрироваться" disabled={!isValid}>Зарегистрироваться</button>
       <p className="form__question">Уже зарегистрированы?
         <a className="form__link"> Войти</a>
       </p>
