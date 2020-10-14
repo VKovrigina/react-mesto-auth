f5d0f5fe19a1ba297451ca515b3194f367416ce7
VKovrigina <v.kovrigina.2001@yandex.ru>
2020-10-14 09:32:03 +0300

    Updates

---
+++ precache-manifest.9b2ca9ee202e2bed1f39729d1d9f0f31.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e868397ae1864e9c9494e9f3478e674a",
+    "url": "/react-mesto-auth/index.html"
+  },
+  {
+    "revision": "2ce6c2bc3f177529673b",
+    "url": "/react-mesto-auth/static/css/main.988ae260.chunk.css"
+  },
+  {
+    "revision": "817ce50df0ba038e852b",
+    "url": "/react-mesto-auth/static/js/2.7af68ee7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-mesto-auth/static/js/2.7af68ee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ce6c2bc3f177529673b",
+    "url": "/react-mesto-auth/static/js/main.53dde82f.chunk.js"
+  },
+  {
+    "revision": "8f938964c218548d1516",
+    "url": "/react-mesto-auth/static/js/runtime-main.1482b755.js"
+  },
+  {
+    "revision": "f10ac534982d6809f220b16907c7a1db",
+    "url": "/react-mesto-auth/static/media/Error.f10ac534.svg"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/react-mesto-auth/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/react-mesto-auth/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "/react-mesto-auth/static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "/react-mesto-auth/static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/react-mesto-auth/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/react-mesto-auth/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/react-mesto-auth/static/media/add-no-bord.b2fce4ac.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/react-mesto-auth/static/media/big-edit.d476a8b8.svg"
+  },
+  {
+    "revision": "d80783439d382b46ce24b8da369767d4",
+    "url": "/react-mesto-auth/static/media/card-delete.d8078343.svg"
+  },
+  {
+    "revision": "e38b395c190826b3bf6d78baff1415a0",
+    "url": "/react-mesto-auth/static/media/check-mark.e38b395c.svg"
+  },
+  {
+    "revision": "be38ad19bc0006f9a08ce139c75dda92",
+    "url": "/react-mesto-auth/static/media/close-icon-little.be38ad19.svg"
+  },
+  {
+    "revision": "5df32a06fdf69f3ae19600017c36d45a",
+    "url": "/react-mesto-auth/static/media/close-icon.5df32a06.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/react-mesto-auth/static/media/edit.6bfdb354.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/react-mesto-auth/static/media/like-active.5bbb05f7.svg"
+  },
+  {
+    "revision": "2b1f7ee54f51648eafe2b619a0378ea4",
+    "url": "/react-mesto-auth/static/media/logo.2b1f7ee5.svg"
+  },
+  {
+    "revision": "83a46f45b0ce22c9a4433c11d0372f26",
+    "url": "/react-mesto-auth/static/media/vector.83a46f45.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./react-mesto-auth/precache-manifest.3f2e707f05792f3f32af5cea6fd47701.js"
+  "/react-mesto-auth/precache-manifest.9b2ca9ee202e2bed1f39729d1d9f0f31.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./react-mesto-auth/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-mesto-auth/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
