03064a98468aa35dbd9d23135d7c16133e8db131
VKovrigina <v.kovrigina.2001@yandex.ru>
2020-10-24 18:32:08 +0300

    fix: current email in menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { CurrentUserEmail } from '../contexts/CurrentUserEmail';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 function Menu({ signOut, isOpen, loggedIn }) {
 
-  const CurrentEmail = React.useContext(CurrentUserEmail);
+  const CurrentUser = React.useContext(CurrentUserContext);
 
   return (
     <>
       { loggedIn && <header className={`menu ${isOpen ? `menu_active` : `menu_inactive`}`}>
         <ul className="menu__container">
-          <li><h1 className="menu__user-info">{ CurrentEmail &&  CurrentEmail}</h1></li>
+          <li><h1 className="menu__user-info">{ CurrentUser &&  CurrentUser.email}</h1></li>
           <li><button onClick={signOut} className="menu__link">Выйти</button></li>
         </ul>
       </header>}
